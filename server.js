4eb05d2d0c0dc71ded95ea679aedd219a62fbd0c
LOGANATHANAN <anloganathan20@gmail.com>
2022-04-17 23:28:43 +0530

    server update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (typeof web3 !== 'undefined') {
     var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'))
 }
 
-const contractAdd="0x919b5F5a92DEd334193D906e691000F07Fa7649b";
+const contractAdd="0x54DCC449329D0444571Ebb2857a15F623C4661b7";
 var contract= new web3.eth.Contract(abi,contractAdd);
 var deployerAddress
 web3.eth.getAccounts().then(function(e){
@@ -80,7 +80,8 @@ catch(err){
 
 app.get('/addThermometer',async (req,res)=>{
   try{
-    await contract.methods.addThermometer().call({from: deployerAddress}).then(function(value){
+    await contract.methods.addThermometer().send({from:deployerAddress,gas:150000}).then(function(value){
+        
         
         console.log(value);
         res.status(201).json({success:true});
@@ -92,6 +93,7 @@ app.get('/addThermometer',async (req,res)=>{
         console.log(e); // prints "This is error message"
         res.status(404).json({ success: false, error: reason });
     });
+    
 }
 catch(err){
   res.status(404).json({ success: false, error: err.message });
@@ -122,6 +124,59 @@ catch(err){
 });
 
 
+app.get('/addPulseOximeter',async (req,res)=>{
+  try{
+    await contract.methods.addPulseOxi().send({from:deployerAddress,gas:150000}).then(function(value){
+        
+        
+        console.log(value);
+        res.status(201).json({success:true});
+    }).catch(e=>{
+        const data = e.data;
+        const txHash = Object.keys(data)[0]; // TODO improve
+        const reason = data[txHash].reason;        
+        console.log("reason"+reason);
+        console.log(e); // prints "This is error message"
+        res.status(404).json({ success: false, error: reason });
+    });
+    
+}
+catch(err){
+  res.status(404).json({ success: false, error: err.message });
+  console.log(err.message);
+}
+});
+
+
+app.get('/listOfPulseOximeters',async (req, res) => {
+  try{
+    await contract.methods.getPulseOxiList().call().then(function(value){
+        
+        console.log(value);
+        res.status(201).json({success:true});
+    }).catch(e=>{
+        const data = e.data;
+        const txHash = Object.keys(data)[0]; // TODO improve
+        const reason = data[txHash].reason;        
+        console.log(reason);
+        console.log(e); // prints "This is error message"
+        res.status(404).json({ success: false, error: reason });
+    });
+}
+catch(err){
+    res.status(404).json({ success: false, err: err.message });
+    console.log(err.message);
+}
+});
+
+
+
+
+app.get('/test',async (req, res) => {
+
+const test =await contract.methods.getThermometerList().call();
+console.log(test)
+})
 app.listen(port, async () => {
   console.log("Express Listening at http://localhost:" + port);
 });
